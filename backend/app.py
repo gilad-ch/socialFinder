301d8b14a75c7b6c584ea3853eb609128b9f9d81
Gilad <oztal0023@gmail.com>
2025-01-23 11:40:27 +0200

    added translator

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-from typing import List, Optional
-from db.twitterDB import TwitterDB, Tweet, User
 from routes import twitter
 from fastapi.middleware.cors import CORSMiddleware
+from deep_translator import GoogleTranslator
 
+translator = GoogleTranslator(source='auto', target='iw')
 app = FastAPI()
 origins = [
     "http://localhost:5173",  # React app's URL during development
@@ -18,5 +17,13 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
-
+@app.get('/api/translate')
+async def translate_text(text: str):
+    try:
+        translated_text = translator.translate(text)
+        return {"original_text": text, "translated_text": translated_text}
+    except Exception as e:
+        raise HTTPException(
+            status_code=400, detail=f"Couldn't translate text: {str(e)}"
+        )
 app.include_router(twitter.router, prefix="/api")
