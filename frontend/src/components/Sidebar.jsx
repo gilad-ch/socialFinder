a7858b79d231ce94326911de1d4683c72d37a4f7
Gilad <oztal0023@gmail.com>
2025-01-27 15:26:04 +0200

    sidebar platform selection added

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,82 +1,19 @@
 import React from "react";
-import { DashboardContext } from "../contexts/DashboardContext";
-import { useState, useEffect, useContext } from "react";
-import { UserCheck, Bookmark, ScanEye, FileSearch2 } from "lucide-react";
-import "../css/Sidebar.css";
-import MultiSelect from "./general/MultiSelect";
-import { fetchKeywords } from "../services/api";
+import { useContext } from "react";
+import { PlatformContext } from "../contexts/PlatformContext";
+import TwitterSidebar from "./Twitter/TwitterSidebar";
 
-function Sidebar() {
-  const { currentStatus, setCurrentStatus, filters, updateFilter } =
-    useContext(DashboardContext);
-  const [keywordList, setKeywordList] = useState([]);
-  const handleKeywordSelected = (selectedKeywords) => {
-    updateFilter("keywords", selectedKeywords);
+function MainContent() {
+  const { selectedPlatform } = useContext(PlatformContext);
+  const renderContent = () => {
+    if (selectedPlatform === "Twitter") {
+      return <TwitterSidebar />;
+    } else {
+      return <TwitterSidebar />;
+    }
   };
 
-  // Fetch keywords on component mount
-  useEffect(() => {
-    fetchKeywords()
-      .then((keywords) => {
-        const keywordList = keywords.map((kw_obj) => kw_obj.keyword);
-        setKeywordList(keywordList);
-      })
-      .catch((error) => console.error("Error fetching keywords:", error));
-  }, []);
-  const statuses = [
-    {
-      name: "Monitored Users",
-      icon: UserCheck,
-      color: "#2196F3",
-      statusCode: 0,
-    },
-    {
-      name: "Keyword Search",
-      icon: FileSearch2,
-      color: "#2196F3",
-      statusCode: 2,
-    },
-    { name: "Saved Tweets", icon: Bookmark, color: "#ff5733", statusCode: 1 },
-  ];
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-
-  const toggleSidebar = () => setIsSidebarOpen(!isSidebarOpen);
-
-  return (
-    <div className="sidebar-section">
-      <button className="sidebar-toggle" onClick={toggleSidebar}>
-        {isSidebarOpen ? "✕" : "☰"}
-      </button>
-      <aside className={`sidebar ${isSidebarOpen ? "open" : ""}`}>
-        <div className="sidebar-header">
-          <h2>Filters menu</h2>
-        </div>
-        <nav className="sidebar-nav">
-          {statuses.map((status) => (
-            <button
-              key={status.statusCode}
-              className={`status-button ${
-                currentStatus === status.statusCode ? "active" : ""
-              }`}
-              onClick={() => setCurrentStatus(status.statusCode)}
-              style={{ "--status-color": status.color }}
-            >
-              <status.icon size={24} />
-              <span>{status.name}</span>
-            </button>
-          ))}
-        </nav>
-        <div className="filters-section">
-          <MultiSelect
-            options={keywordList}
-            selectedOptions={filters.keywords}
-            placeholder="Keywords Filter..."
-            onChange={handleKeywordSelected}
-          />
-        </div>
-      </aside>
-    </div>
-  );
+  return renderContent();
 }
 
-export default Sidebar;
+export default MainContent;
