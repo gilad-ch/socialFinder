0acd84e870dd39eb19dad94800b15696ff63f484
RexBex555 <oztal0023@gmail.com>
2025-01-29 10:55:50 +0200

    added CLI interaval argument to scheduler

--- worker/twitter/scheduler.py
+++ worker/twitter/scheduler.py
@@ -3,7 +3,7 @@ import logging
 from twitterTask import insert_keyword_tweets, insert_user_tweets
 from twitter_utils.twitterDB import TwitterDB
 import asyncio
-import os
+import argparse
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 logger = logging.getLogger(__name__)
@@ -24,12 +24,30 @@ async def run_tasks():
         insert_keyword_tweets.delay(keyword['_id'])
 
 
-async def start_scheduler():
+async def start_scheduler(interval):
     scheduler = AsyncIOScheduler()
     # scheduler interval time
-    scheduler.add_job(run_tasks, 'interval', hours=24)
+    scheduler.add_job(run_tasks, 'interval', minutes=interval)
     scheduler.start()
     await asyncio.Event().wait()
 
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Run a scheduler that executes tasks at intervals.")
+
+    # Add the interval argument (in minutes)
+    parser.add_argument(
+        "--interval",
+        type=int,
+        default=1440,  # Default interval is (24 hours)
+        help="Interval in minutes for scheduling tasks (default is 1440 minutes)."
+    )
+
+    args = parser.parse_args()
+
+    asyncio.run(start_scheduler(interval=args.interval))
+
+
 if __name__ == "__main__":
-    asyncio.run(start_scheduler())
+    main()
